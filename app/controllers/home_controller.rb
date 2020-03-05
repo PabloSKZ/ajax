6c0e85de22e1e8f2f3e19eb428c2cae8cdedfe9d
PabloSKZ <pablo.eskenazi@gmail.com>
2020-03-05 22:31:50 +0100

    ok

--- app/controllers/home_controller.rb
+++
@@ -1,6 +0,0 @@
-class HomeController < ApplicationController
-  def index
-    @tasks = Task.all
-    @categories = Category.all
-  end
-end
--- db/migrate/20181116110124_add_imageto_task.rb
+++
@@ -1,5 +0,0 @@
-class AddImagetoTask < ActiveRecord::Migration[5.2]
-  def change
-    add_column :tasks, :image, :string
-  end
-end
